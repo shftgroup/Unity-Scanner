a1dafbe40079ca12b4ea16a1468d07d9ef1dec48
jsj5909 <jsj5909@hotmail.com>
2021-08-27 17:00:51 -0700

    Changed window size

--- src/main/kotlin/com/example/MyApp.kt
+++ src/main/kotlin/com/example/MyApp.kt
@@ -1,6 +1,18 @@
 package com.example
 
 import com.example.view.MainView
+import javafx.stage.Stage
 import tornadofx.App
 
 class MyApp: App(MainView::class, Styles::class)
+{
+    override fun start(stage: Stage) {
+        with(stage)
+        {
+            width = 1200.0
+            height = 600.0
+        }
+
+        super.start(stage)
+    }
+}
