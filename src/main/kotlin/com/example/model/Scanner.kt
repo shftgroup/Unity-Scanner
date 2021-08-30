883760200c4c2220a463bd06b0c8fe1331937f2d
jsj5909 <jsj5909@hotmail.com>
2021-08-30 10:18:25 -0700

    Updated UI

---
+++ src/main/kotlin/com/example/model/Scanner.kt
@@ -0,0 +1,16 @@
+package com.example.model
+import tornadofx.*
+
+class Scanner {
+
+    val directory = chooseDirectory()
+
+    lateinit var editorVersion:String
+
+    fun ExtractVersion()
+    {
+        
+    }
+
+
+}
--- src/main/kotlin/com/example/view/MainView.kt
+++ src/main/kotlin/com/example/view/MainView.kt
@@ -1,12 +1,53 @@
 package com.example.view
 
 import com.example.Styles
+import com.example.model.Scanner
+import javafx.scene.control.TabPane
 import tornadofx.*
 
+
 class MainView : View("Hello TornadoFX") {
-    override val root = hbox {
-        label(title) {
+
+    lateinit var projectScanner:Scanner
+
+    override val root = vbox {
+        menubar {
+            menu("File") {
+                item("Open Project")
+                {
+                    setOnAction { projectScanner = Scanner() }
+                }
+                item("Save")
+                item("Quit")
+            }
+            menu("Edit") {
+                item("Copy")
+                item("Paste")
+            }
+        }
+
+        label("Editor Version: ") {
             addClass(Styles.heading)
         }
+        tabpane {
+
+
+            tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
+
+            tab("Build") {
+                disableClose()
+            }
+            tab("Packages") {
+
+                }
+            tab("Assets") {
+
+            }
+
+            }
+
     }
-}
+        }
+
+
+
